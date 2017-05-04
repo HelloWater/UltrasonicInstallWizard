65b6ec5728c0dec86fea9cd28cd29fbd9dc46d60
HelloWater <qulonghui2008@126.com>
2017-05-04 10:21:48 +0800

    UI Update Thread

--- UltrasonicInstallWizard/Page_Progress.xaml.cs
+++ UltrasonicInstallWizard/Page_Progress.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace UltrasonicInstallWizard
 {
@@ -20,6 +21,9 @@ namespace UltrasonicInstallWizard
     /// </summary>
     public partial class Page_Progress : BasePage
     {
+        private delegate void DelegateSetProgress();
+        private bool m_stopFlag = false;
+
         public Page_Progress()
         {
             InitializeComponent();
@@ -31,6 +35,7 @@ namespace UltrasonicInstallWizard
         }
         private void btnClose_ClickEvent(object sender, MouseButtonEventArgs e)
         {
+            m_stopFlag = true;
             ParentWindow.Close();
             //Application.Current.Shutdown();
             //Environment.Exit(0);
@@ -38,7 +43,28 @@ namespace UltrasonicInstallWizard
         private void btnNextStep_ClickEvent(object sender, MouseButtonEventArgs e)
         {
             //MessageBox.Show("Next Step");
-            ParentWindow.NavigationCall("Page_Sys_Env"); //navigate
+            //ParentWindow.NavigationCall("Page_Sys_Env"); //navigate
+
+            m_stopFlag = !m_stopFlag;
+            if(!m_stopFlag)
+            {
+                Thread thread = new Thread(new ThreadStart(refreshProgress));
+                thread.Start();
+            }
+        }
+        private void refreshProgress()
+        {
+            while(!m_stopFlag)
+            {
+                //向UI界面更新显示
+                Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.SystemIdle,
+                    new DelegateSetProgress(setCurProgress));
+            }
+        }
+        private void setCurProgress()
+        {
+            string currentTime = System.DateTime.Now.ToString();
+            m_sProgress.Text = currentTime;
         }
     }
 }
